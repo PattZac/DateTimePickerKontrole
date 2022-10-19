5c9e0d4001ca1d9c8860cf6557e28f60554626d8
Pat <113007904+PattZac@users.noreply.github.com>
2022-10-19 09:22:28 +0200

    Add files via upload

---
+++ DateTimePickerKontrole/DateTimePickerKontrole/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DateTimePickerKontrole
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "Vaš datum leta je:\n" + dateTimePicker1.Text + "\nVaše vrijeme polaska je:\n" + dateTimePicker2.Text;
+        }
+    }
+}
